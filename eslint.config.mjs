b41397c812baf5f100e10f48b4eaa0f748a42846
okryzhanovska <77673057+ohalytska-ui@users.noreply.github.com>
2024-10-15 15:15:56 +0200

    feat: initial commit & app set up

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      semi: 'error',
+      'no-console': 'warn',
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+      'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+      'no-duplicate-imports': 'error',
+      'no-nested-ternary': 'warn',
+      curly: 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  { plugins: ['prettier'] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
